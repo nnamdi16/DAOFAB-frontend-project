e83d25a337a5b1b51fccae8558815886f48bf1e8
Nnamdi <nnwabuokei@paga.com>
2020-11-18 17:40:05 +0100

    implemented fetching data from the db using HttpClient and Observables

--- src/app/services/transactions.service.ts
+++ src/app/services/transactions.service.ts
@@ -1,9 +1,28 @@
+import { Transactions } from './../models/Transactions';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http'
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-type':'application/json',
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionsService {
+  transactionDetailsUrl:string=`http://localhost:3000/transactionDetails/`
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  fetchTransactionDetails(page:string): Observable<Transactions>{
+    let params = new  HttpParams().set('page',page);
+    console.log('We are here')
+    const result= this.http.get<Transactions>(this.transactionDetailsUrl,{params});
+    console.log(result);
+    return result;
+
+  }
 }
