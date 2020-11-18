c6fd28aa4c7f3f8612adbfdb24c97ab440ccf548
Nnamdi <nnwabuokei@paga.com>
2020-11-18 17:33:20 +0100

    Added all dependency for making http (HttpClientModule) calls and implmenting angular material table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatInputModule} from '@angular/material/input';
-import {MatTableModule} from '@angular/material/table'
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { TransactionsComponent } from './components/transactions/transactions.component';
+
 
 @NgModule({
   declarations: [
@@ -24,7 +27,11 @@ import { TransactionsComponent } from './components/transactions/transactions.co
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    HttpClientModule,
+    FormsModule,
+    MatNativeDateModule,
+    ReactiveFormsModule
     
   ],
   providers: [],
