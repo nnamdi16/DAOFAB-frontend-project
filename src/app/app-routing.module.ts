8cfccd60b87273f126f210313fe7304f080dd1f5
Nnamdi <nnwabuokei@paga.com>
2020-11-18 17:29:28 +0100

    Added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { TransactionsComponent } from './components/transactions/transactions.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/home',pathMatch:'full'},
+  {path:'home', component:TransactionsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
