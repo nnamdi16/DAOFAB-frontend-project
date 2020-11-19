41b0fd1b804234e72043020bde69fa23d74ce2b8
Nnamdi <nnwabuokei@paga.com>
2020-11-19 08:25:04 +0100

    created child-transaction component

---
+++ src/app/components/child-transaction/child-transaction.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child-transaction',
+  templateUrl: './child-transaction.component.html',
+  styleUrls: ['./child-transaction.component.css']
+})
+export class ChildTransactionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
