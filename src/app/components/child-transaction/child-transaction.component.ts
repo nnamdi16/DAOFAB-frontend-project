276ff3325e79d63543f76367b5160cd779a0bce5
Nnamdi <nnwabuokei@paga.com>
2020-11-20 22:28:08 +0100

    used the ActivatedRoute to get the id of the route and passed it to the fetchChildTransaction method to get the child transaction details based on the parent id.

--- src/app/components/child-transaction/child-transaction.component.ts
+++ src/app/components/child-transaction/child-transaction.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { ChildTransaction, ChildTransactionDetails } from './../../models/Transactions';
+import { TransactionsService } from './../../services/transactions.service';
 
 @Component({
   selector: 'app-child-transaction',
@@ -6,10 +11,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./child-transaction.component.css']
 })
 export class ChildTransactionComponent implements OnInit {
-
-  constructor() { }
+  childTransactionDetails:ChildTransactionDetails;
+  childTransactions$: Observable<ChildTransactionDetails>
+  dataSource = new MatTableDataSource<ChildTransaction>();
+  displayedColumns:string[] =["id" ,"parentId", "paidAmount"];
+  page:string="1"
+  
+  constructor(private transactionService: TransactionsService, private router:Router, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.fetchChildTransactionDetails(id);
+    console.log(id);
+    console.log(this.childTransactions$);
+  
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+  
+  fetchChildTransactionDetails(id:string) {
+    console.log('We are here');
+    this.transactionService.fetchChildTransactionDetails(id).subscribe(childTransactionDetails => {
+      this.childTransactionDetails = childTransactionDetails;
+      console.log(this.childTransactionDetails);
+      this.dataSource = new MatTableDataSource<ChildTransaction>(childTransactionDetails.data);
+    })
+    return of(this.childTransactionDetails);
   }
 
 }
