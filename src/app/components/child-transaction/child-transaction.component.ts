7d52fd1c768043879933fc81d2350d183eda3550
Nnamdi <nnwabuokei@paga.com>
2020-11-21 22:58:31 +0100

    removed zombie codes and added column headers to the displayedColumn array and added sorting implementation

--- src/app/components/child-transaction/child-transaction.component.ts
+++ src/app/components/child-transaction/child-transaction.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
-import { of } from 'rxjs';
-import { ChildTransaction, ChildTransactionDetails } from './../../models/Transactions';
+import { ChildTransaction } from './../../models/Transactions';
 import { TransactionsService } from './../../services/transactions.service';
 
 @Component({
@@ -11,16 +11,25 @@ import { TransactionsService } from './../../services/transactions.service';
   styleUrls: ['./child-transaction.component.css']
 })
 export class ChildTransactionComponent implements OnInit {
-  childTransactionDetails:ChildTransactionDetails;
   dataSource = new MatTableDataSource<ChildTransaction>();
-  displayedColumns:string[] =["id" ,"parentId", "paidAmount"];
+  displayedColumns:string[] =["id", "sender", "receiver","totalAmount", "paidAmount"];
   page:string="1"
+  @ViewChild(MatSort) sort: MatSort;
   
   constructor(private transactionService: TransactionsService, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     this.fetchChildTransactionDetails(id);
+  }
+
+
+    /**
+   * Set the paginator and sort after the view init since this component will
+   * be able to query its view for the initialized paginator and sort.
+   */
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
   }
 
   applyFilter(filterValue: string) {
@@ -31,10 +40,9 @@ export class ChildTransactionComponent implements OnInit {
   
   fetchChildTransactionDetails(id:string) {
     this.transactionService.fetchChildTransactionDetails(id).subscribe(childTransactionDetails => {
-      this.childTransactionDetails = childTransactionDetails;
-      this.dataSource = new MatTableDataSource<ChildTransaction>(childTransactionDetails.data);
+      const{data} = childTransactionDetails;
+      this.dataSource = new MatTableDataSource<ChildTransaction>(data);
     })
-    return of(this.childTransactionDetails);
   }
 
 }
