91c7a1e6f2852ad804b55881be6965df23fdf98e
Nnamdi <nnwabuokei@paga.com>
2020-11-21 14:19:19 +0100

    created pagination method impmenting pagination, previousPagination and nextPagination for implementing pagination when required to move to the previous page or next page

--- src/app/components/transactions/transactions.component.ts
+++ src/app/components/transactions/transactions.component.ts
@@ -12,20 +12,18 @@ import { TransactionsService } from './../../services/transactions.service';
   styleUrls: ['./transactions.component.css']
 })
 export class TransactionsComponent implements OnInit {
+  pages:string[]=[];
+  currentPage:number;
+  totalPages:number;
   loading: boolean = true;
   paginationDetails: PaginationDetails;
   pageSize:number=2;
   transactionDetails:TransactionDetails[];
   sam:Transactions
   displayedColumns:string[] =["id", "sender", "receiver", "totalAmount", "paidAmount"];
-  // dataSource:  MatTableDataSource<TransactionDetails>;
   dataSource = new  MatTableDataSource<TransactionDetails>();
-  page:number=1
+  page:string="1"
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  // @ViewChild(MatPaginator) 
-  // set paginator(value: MatPaginator) {
-  //   this.dataSource.paginator = value;
-  // }
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(private transactionsService:TransactionsService) {}
@@ -45,8 +43,6 @@ export class TransactionsComponent implements OnInit {
    * be able to query its view for the initialized paginator and sort.
    */
   ngAfterViewInit() {
-    // this.dataSource.paginator = this.paginator
-    // this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
 
@@ -56,18 +52,35 @@ export class TransactionsComponent implements OnInit {
     this.dataSource.filter = filterValue;
   }
 
-  getTransactionDetails(page:number) {
+  pagination(paginationResponse) {
+    const{totalPages,currentPage} = paginationResponse;
+    this.totalPages = totalPages;
+    this.currentPage = currentPage;
+    let pages:number[] = [];
+    let pageValue = 1;
+    for (let index = 0; index < totalPages; index++) {
+      pages.push(pageValue);
+      pageValue++;
+    }
+    console.log(pages.join().split(','));
+    console.log(paginationResponse);
+    this.pages = pages.join().split(',');
+    return this.pages;
+  }
+
+  getTransactionDetails(page:string) {
     console.log('We are here');
-    this.transactionsService.fetchTransactionDetails(page.toString())
+    this.transactionsService.fetchTransactionDetails(page)
       .subscribe(transactionDetails =>{
         this.paginationDetails = transactionDetails.paginationResponse
         console.log(this.paginationDetails.totalItems);
         console.log(transactionDetails.data);
         this.dataSource = new MatTableDataSource<TransactionDetails>(transactionDetails.data);
+        this.pagination(transactionDetails.paginationResponse);
         // this.dataSource = new MatTableDataSource<TransactionDetails>(transactionDetails.data);
         this.transactionDetails = transactionDetails.data;
         this.pageSize = transactionDetails.data.length;
-        console.log(this.paginationDetails);
+        console.log(this.paginationDetails); 
         console.log(this.dataSource);
         console.log(this.transactionDetails);
         this.dataSource.paginator = this.paginator;
@@ -77,13 +90,35 @@ export class TransactionsComponent implements OnInit {
   pageChanged($event) {
     this.loading = true;
     console.log($event);
-    const{pageIndex} = $event;
-    this.getTransactionDetails(pageIndex+1);
-    // let pageIndex = event.pageIndex;
-    // let pageSize = event.pageSize;
-    // console.log(pageIndex,pageSize);
-    // this.getTransactionDetails(pageIndex+1);
+    return this.getTransactionDetails($event);
   }
+
+  previousPagination() {
+    console.log(this.currentPage);
+    if(this.currentPage === 1) {
+      return this.getTransactionDetails(this.currentPage.toString());
+    }
+    this.currentPage--;
+    console.log(this.currentPage);
+    return this.getTransactionDetails(this.currentPage.toString())
+
+  }
+
+  nextPagination() {
+    console.log(this.currentPage);
+    if(this.currentPage === this.totalPages) {
+      return this.getTransactionDetails(this.currentPage.toString());
+    }
+    this.currentPage++;
+    console.log(this.currentPage);
+    return this.getTransactionDetails(this.currentPage.toString())
+
+  }
+
+
+
+
+
 
 
 
