d10caac85b9cb528883cc5db673dc30cdbf11bd9
Nnamdi <nnwabuokei@paga.com>
2020-11-22 21:15:02 +0100

    added previousPage and nextPage property, refactored the previousPagination method and nextPagination method

--- src/app/components/transactions/transactions.component.ts
+++ src/app/components/transactions/transactions.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TransactionDetails } from './../../models/Transactions';
 import { TransactionsService } from './../../services/transactions.service';
 
@@ -14,20 +15,22 @@ export class TransactionsComponent implements OnInit {
   isLoading:boolean= true;
   pages:string[]=[];
   currentPage:number;
+  previousPage:number;
+  nextPage:number;
   totalPages:number;
   loading: boolean = true;
-  displayedColumns:string[] =["id", "sender", "receiver", "totalAmount", "paidAmount"];
+  displayedColumns:string[] = ["id", "sender", "receiver", "totalAmount", "paidAmount"];
   dataSource = new  MatTableDataSource<TransactionDetails>();
-  page:string="1"
+  page:string="1";
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(private transactionsService:TransactionsService) {}
 
   ngOnInit(): void {
-    this.getTransactionDetails(this.page); 
+    this.getTransactionDetails(this.page);    
   }
 
-    /**
+  /**
    * Set the paginator and sort after the view init since this component will
    * be able to query its view for the initialized paginator and sort.
    */
@@ -45,6 +48,8 @@ export class TransactionsComponent implements OnInit {
     const{totalPages,currentPage} = paginationResponse;
     this.totalPages = totalPages;
     this.currentPage = currentPage;
+    this.currentPage === 1 ? this.previousPage = 1 :this.previousPage=this.currentPage-1;
+    this.currentPage === totalPages ? this.nextPage = totalPages :this.nextPage=this.currentPage+1;
     let pages:number[] = [];
     let pageValue = 1;
     for (let index = 0; index < totalPages; index++) {
@@ -72,20 +77,22 @@ export class TransactionsComponent implements OnInit {
   }
 
   previousPagination() {
+    this.currentPage === 1 ? this.previousPage = 1 :this.previousPage=this.currentPage-1;
     if(this.currentPage === 1) {
-      return this.getTransactionDetails(this.currentPage.toString());
+      return this.getTransactionDetails(this.previousPage.toString());
     }
     this.currentPage--;
-    return this.getTransactionDetails(this.currentPage.toString())
+    return this.getTransactionDetails(this.previousPage.toString())
 
   }
 
   nextPagination() {
+    this.currentPage === this.totalPages ? this.nextPage = this.totalPages :this.nextPage=this.currentPage+1;
     if(this.currentPage === this.totalPages) {
-      return this.getTransactionDetails(this.currentPage.toString());
+      return this.getTransactionDetails(this.nextPage.toString());
     }
     this.currentPage++;
-    return this.getTransactionDetails(this.currentPage.toString())
+    return this.getTransactionDetails(this.nextPage.toString())
 
   }
 
